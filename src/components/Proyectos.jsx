2fcb2c8673f8f9bc41f1916d4fe3d9d55b57813e
Alejo-Vignale <138889322+Alejo-Vignale@users.noreply.github.com>
2024-02-05 19:41:42 -0300

    project

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./CSS/Proyectos.css";
 
 function Proyectos() {
-  
   const projects = [
     {
       name: "Weather APp",
@@ -13,37 +12,52 @@ function Proyectos() {
     {
       name: "Project 2",
       description: "Description of Project 2",
-      url: "https://example.com/project2",
+      url: "https://game-whack-a-mole.vercel.app/",
     },
     {
       name: "Project 2",
       description: "Description of Project 2",
-      url: "https://example.com/project2",
+      url: "https://game-snake-rust.vercel.app/",
     },
     {
       name: "Project 2",
       description: "Description of Project 2",
-      url: "https://example.com/project2",
+      url: "https://to-do-list-two-bice.vercel.app/",
     },
     {
       name: "Project 2",
       description: "Description of Project 2",
-      url: "https://example.com/project2",
+      url: "https://popup-wheat.vercel.app/",
     },
     {
       name: "Project 2",
       description: "Description of Project 2",
-      url: "https://example.com/project2",
+      url: "https://notes-app-eight-lovat.vercel.app/",
     },
     {
       name: "Project 2",
       description: "Description of Project 2",
-      url: "https://example.com/project2",
+      url: "https://calculator-lake-phi.vercel.app/",
     },
     {
       name: "Project 2",
       description: "Description of Project 2",
-      url: "https://example.com/project2",
+      url: "https://music-player-six-tau.vercel.app/",
+    },
+    {
+      name: "Project 2",
+      description: "Description of Project 2",
+      url: "https://quizz-app-gilt.vercel.app/",
+    },
+    {
+      name: "Project 2",
+      description: "Description of Project 2",
+      url: "https://random-password-one.vercel.app/",
+    },
+    {
+      name: "Project 2",
+      description: "Description of Project 2",
+      url: "https://toastify-notification.vercel.app/",
     },
   ];
 
